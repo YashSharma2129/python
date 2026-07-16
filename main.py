8f39b188fa61b53ecd8905aab104b0e471174e2f
Yash Sharma <yash25578@gmail.com>
2026-07-17 03:10:17 +0530

    add prime number

--- main.py
+++ main.py
@@ -214,3 +214,15 @@
 #         print("Perfect number")
 # else:
 #         print("Not a perfect number")
+
+## Check prime number
+# n=int(input("Enter your number: "))
+# if(n<=1):
+#     print("Not a prime number")
+# else:
+#     for i in range(2,n):
+#         if(n%i==0):
+#             print("Not a prime number")
+#             break
+#     else:
+#             print("Prime number")
