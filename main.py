9036b5283a40444eaf14d6e4d0a99c527f2034e5
Yash Sharma <yash25578@gmail.com>
2026-07-17 04:09:08 +0530

    count char, digits, symbols

--- main.py
+++ main.py
@@ -247,3 +247,28 @@
 #     print("Palindrome")
 # else:
 #     print("Not a palindrome")
+
+str1="P@#yn26at^&i5ve"
+print(type(str1))
+# char=0; digits=0; symbol=0
+# for i in str1:
+#     if(i.isalpha()):
+#         char+=1
+#     elif(i.isdigit()):
+#         digits+=1
+#     else:
+#         symbol+=1
+# print("Characters: ", char, "Digits: ", digits, "Symbols: ", symbol)
+
+# count_char=0; count_digit=0; count_symbol=0
+# for ch in str1:
+#     if ('A' <= ch <= 'Z' or 'a' <= ch <= 'z'):
+#         print(ch, "is a character")
+#         count_char+=1
+#     elif ('0' <= ch <= '9'):
+#         print(ch, "is a digit")
+#         count_digit+=1
+#     else:
+#         print(ch, "is a symbol")
+#         count_symbol+=1
+# print("Characters: ", count_char, "Digits: ", count_digit, "Symbols: ", count_symbol)
