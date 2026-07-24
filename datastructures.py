ac6fad53e8ab1bcd814127c967f568e76e235bdb
Yash Sharma <yash25578@gmail.com>
2026-07-24 08:03:45 +0530

    perform list operations same as string

--- datastructures.py
+++ datastructures.py
@@ -1,7 +1,21 @@
-## Data ko structure way me present krne ko datastructures kehte hai.
-## Data ko efficiently store krne aur access krne ke liye data structures ka use kiya jata hai.
+# Data ko structure way me present krne ko datastructures kehte hai.
+# Data ko efficiently store krne aur access krne ke liye data structures ka use kiya jata hai.
 # list,tupple,dict,set ye 4 data structures hai.
 # custom data structures bhi banaye ja sakte hai. stack,queue,linked list,tree,graph ye data structures bhi hai.
+
+## List data structure. 
+# ye mutable data structure hai. ap kisi bhi value ko change kar sakte ho.
+# list me duplicate values store ki ja sakti hai. same value accuring multiple times in the list.
+# list me indexing hoti hai. list me heterogeneous data store kiya ja sakta hai. we can save different types of data in the same list.like int,float,string etc.
+# ordered data structure hai. list me order maintain hota hai. list me values ko access karne ke liye indexing ka use kiya jata hai.
+
+# a=[12,4,5,6,7]
+# fruits=['apple','banana','mango','grapes']
+# print(a[0]) ## 12
+# print(fruits[1]) ## banana
+# print(a[-1]) ## 7
+# print(fruits[0:3]) ## ['apple', 'banana', 'mango']
+
 
 # a=12,4,5,6,6
 # print(a) ## tuple data structure
