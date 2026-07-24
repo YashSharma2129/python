4311093d56275ec64be006eea5b9906c3262636f
Yash Sharma <yash25578@gmail.com>
2026-07-24 07:39:13 +0530

    Add Python functions practice

---
+++ functions.py
@@ -0,0 +1,23 @@
+## def -- -> define function name {}:
+
+# def yash():
+#     print("Hello my name is Yash and my age is 21")
+# yash()
+
+# function parameters  -- -> the thing you accept in the function is parameters 
+# def sum(a,b):
+#     print(f"the sum of the numbers is:{a+b}")
+
+# sum(12, 13) ## arguments - --> the thing you pass or provide in the function is arguments
+
+# def hello(name, age):
+#     print(f"Hello my name is {name} and my age is {age}")
+# # hello("Yash", 21)
+# hello(21, "Yash") ## positional arguments
+# hello(age=21, name="Yash") ## keyword arguments
+
+# def sum(a, b=45): ## default arguments
+#     print(f"the sum of the numbers is: {a + b}")
+# sum(12,13) ## positional arguments and value of b is replaced by 13
+# sum(12) ## uses default value for b
+
