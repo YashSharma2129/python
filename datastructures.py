cb9f9eb4502b84f41b0069e503b6ac67d1acfa12
Yash Sharma <yash25578@gmail.com>
2026-07-24 07:52:20 +0530

    learning data structure

---
+++ datastructures.py
@@ -0,0 +1,7 @@
+## Data ko structure way me present krne ko datastructures kehte hai.
+## Data ko efficiently store krne aur access krne ke liye data structures ka use kiya jata hai.
+# list,tupple,dict,set ye 4 data structures hai.
+# custom data structures bhi banaye ja sakte hai. stack,queue,linked list,tree,graph ye data structures bhi hai.
+
+# a=12,4,5,6,6
+# print(a) ## tuple data structure
